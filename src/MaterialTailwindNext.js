f5e2ba6dff576f78591a6d9b005e31a71f98e20c
RishabhRV4444 <rishabh@arevei.com>
2024-09-25 16:37:55 +0530

    second

---
+++ src/MaterialTailwindNext.js
@@ -0,0 +1,5 @@
+"use client";
+ 
+import { ThemeProvider,Typography,Carousel, Navbar, MobileNav, Button, Menu, MenuHandler, MenuList, MenuItem, Avatar, Card, IconButton,Input} from "@material-tailwind/react";
+ 
+export {  ThemeProvider,Typography,Carousel, Navbar, MobileNav, Button, Menu, MenuHandler, MenuList, MenuItem, Avatar, Card, IconButton,Input};
---
+++ src/components/HomePage/Category.js
@@ -0,0 +1,29 @@
+// components/ProductCategories.js
+export default function ProductCategories() {
+        const categories = [
+          { name: 'Pendants', image: '/images/pendants.png' },
+          { name: 'Earing', image: '/images/earings.png' },
+          { name: 'Necklace', image: '/images/necklaces.png' },
+          { name: 'Bracelets', image: '/images/bracelets.png' },
+          { name: 'Sets', image: '/images/sets.png' },
+          { name: 'Anklets', image: '/images/anklets.png' },
+        ];
+      
+        return (
+          <div className="container mx-auto py-8">
+            <div className="grid grid-cols-6 gap-6">
+              {categories.map((category) => (
+                <div key={category.name} className="text-center">
+                  <img
+                    src={category.image}
+                    alt={category.name}
+                    className="w-24 h-24 mx-auto mb-2"
+                  />
+                  <h3 className="text-gray-600">{category.name}</h3>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      }
+      
